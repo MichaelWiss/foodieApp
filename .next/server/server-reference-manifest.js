fc678c50aac6cb448dbc65d2cd8dc87b2e75964a
MichaelWiss <michael.wiss@gmail.com>
2024-03-05 19:26:24 -0600

    initial commit

---
+++ .next/server/server-reference-manifest.js
@@ -0,0 +1,1 @@
+self.__RSC_SERVER_MANIFEST="{\n  \"node\": {},\n  \"edge\": {},\n  \"encryptionKey\": \"5xfCFF1uQEkKblX7RKuhUst+B8WGIUMydDeRX/MkNi0=\"\n}"
---
+++ app/about/page.js
@@ -0,0 +1,11 @@
+
+const AboutPage = () => {
+  return (
+    <main>
+        <h1>AboutPage</h1>
+    </main>
+    
+  )
+}
+
+export default AboutPage;
