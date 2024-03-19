0a041cc1fffb5c714275fc6831b611d50faac47a
MichaelWiss <michael.wiss@gmail.com>
2024-03-19 16:50:03 -0500

    image preview

--- components/meals/image-picker.js
+++ components/meals/image-picker.js
@@ -1,16 +1,38 @@
+'use client'
+import { useRef } from 'react';
 import classes from './image-picker.module.css';
 
-export default function ImagePicker({label, name}) {
+export default function ImagePicker({ label, name }) {
+    const [pickedImage, setPickedImage] = useState();
+    const imageInput = useRef();
+
+    function handlePickClick() {
+        imageInput.current.click();
+    }
+
+    function handleImageChange(event) {
+        const file = event.target.files[0];
+    }
   return (
     <div className={classes.picker}>
         <label htmlFor={name}></label>
         <div className={classes.controls}>
-            <input 
+            <input
+                className={classes.input}
                 type="file" 
                 id={name} 
                 accept="image/png, image/jpeg" 
                 name={name}
-            />    
+                ref={imageInput}
+                onChange={handleImageChange}
+            />  
+            <button 
+                className={classes.button} 
+                type="button" 
+                onClick={handlePickClick}
+                >
+                 Pick an Image
+            </button>  
         </div>
     </div>
   );
