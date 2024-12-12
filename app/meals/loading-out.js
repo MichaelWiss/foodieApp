8004fa192a0848aba1b007b68bd04c3a31726d27
MichaelWiss <michael.wiss@gmail.com>
2024-12-12 15:22:28 -0600

    update

--- app/meals/loading-out.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import classes from './loading.module.css';
-
-export default function MealsLoadingPage() {
-  return (
-    <p className={classes.loading}>Fetching meals...</p>
-  )
-}
