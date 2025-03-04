ec5276ec363dbe1abe5482a723ae056739e8cc02
MichaelWiss <michael.wiss@gmail.com>
2025-03-04 15:50:20 -0600

    s3 bucket

--- next.config.js
+++ next.config.js
@@ -1,10 +1,24 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'michaelwiss-foodie-users-image.s3.us-east-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
 
 module.exports = {
     experimental: {
       serverActions: {
-        bodySizeLimit: '2mb',
+        bodySizeLimit: '3mb',
       },
     },
   }
+
+
+  
