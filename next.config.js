cd29ddd1f3e4ed7cd0eee57a9008cad4ec365e85
MichaelWiss <michael.wiss@gmail.com>
2025-03-05 19:03:04 -0600

    Untrack files in .gitignore

---
+++ next.config.js
@@ -0,0 +1,24 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'michaelwiss-foodie-users-image.s3.us-east-2.amazonaws.com',
+        port: '',
+        pathname: '/**',
+      },
+    ],
+  },
+};
+
+module.exports = {
+    experimental: {
+      serverActions: {
+        bodySizeLimit: '3mb',
+      },
+    },
+  }
+
+
+  
