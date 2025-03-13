9f5afb69a6316e008e98665a3b94f26dcc13073d
MichaelWiss <michael.wiss@gmail.com>
2025-03-12 20:07:36 -0500

    aws bug

--- next.config.js
+++ next.config.js
@@ -1,10 +1,9 @@
-/** @type {import('next').NextConfig} */
 const nextConfig = {
   images: {
     remotePatterns: [
       {
         protocol: 'https',
-        hostname: 'https://michaelwiss-foodie-users-image.s3.us-east-2.amazonaws.com',
+        hostname: 'michaelwiss-foodie-users-image.s3.us-east-2.amazonaws.com',
         port: '',
         pathname: '/**',
       },
